4524b131e83cdb256bc06ebf29935620f833d3d5
John Tucker <jtucker@appirio.com>
2019-09-06 16:13:44 -0400

    State

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component }  from 'react';
 
-const State = () => {
-  return (
-    <div>
-      <h3>State</h3>
-      <div>TBD</div>
-    </div>
-  );
+export default class State extends Component {
+  state = {
+    value: 0,
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <div>
+        <h3>State</h3>
+        <div>{value}</div>
+        <button onClick={this.handleClick}>Increment</button>
+      </div>
+    );
+  }
+
+  handleClick = () => {
+    this.setState(({ value }) => ({
+      value: value + 1, 
+    }));
+  }
 }
-
-export default State;
--- src/components/StateHook.jsx
+++ src/components/StateHook.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
 const StateHook = () => {
+  const [value, setValue] = useState(0);
+  const handleClick = useCallback(() => {
+    setValue(value + 1);
+  }, [setValue, value]);
+
   return (
     <div>
       <h3>State Hook</h3>
-      <div>TBD</div>
+      <div>{value}</div>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
