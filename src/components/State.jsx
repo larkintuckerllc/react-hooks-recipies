1af12f3453a427d71676b21c14267877b304d039
John Tucker <jtucker@appirio.com>
2019-09-07 05:34:42 -0400

    Finish State

--- src/components/State.jsx
+++ src/components/State.jsx
@@ -3,16 +3,19 @@ import React, { Component }  from 'react';
 export default class State extends Component {
   state = {
     value: 0,
+    value2: 0,
   };
 
   render() {
-    const { value } = this.state;
+    const { value, value2 } = this.state;
     return (
       <div>
         <h3>State</h3>
         <div>{value.toString()}</div>
         <button onClick={this.handleIncrementClick}>Increment</button>
         <button onClick={this.handleResetClick}>Reset</button>
+        <div>{value2.toString()}</div>
+        <button onClick={this.handleIncrement2Click}>Increment2</button>
       </div>
     );
   }
@@ -20,6 +23,12 @@ export default class State extends Component {
   handleIncrementClick = () => {
     this.setState(({ value }) => ({
       value: value + 1, 
+    }));
+  };
+
+  handleIncrement2Click = () => {
+    this.setState(({ value2 }) => ({
+      value2: value2 + 1, 
     }));
   };
 
--- src/components/StateHook.jsx
+++ src/components/StateHook.jsx
@@ -2,9 +2,13 @@ import React, { useCallback, useState } from 'react';
 
 const StateHook = () => {
   const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(0);
   const handleIncrementClick = useCallback(() => {
-    setValue(value => value + 1);
+    setValue(prev => prev + 1);
   }, [setValue]);
+  const handleIncrement2Click = useCallback(() => {
+    setValue2(prev => prev + 1);
+  }, [setValue2]);
   const handleResetClick = useCallback(() => {
     setValue(0);
   }, [setValue]);
@@ -15,6 +19,8 @@ const StateHook = () => {
       <div>{value}</div>
       <button onClick={handleIncrementClick}>Increment</button>
       <button onClick={handleResetClick}>Reset</button>
+      <div>{value2}</div>
+      <button onClick={handleIncrement2Click}>Increment2</button>
     </div>
   );
 }
